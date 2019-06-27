8713efb5fa52e573942e75324badac14dbaadccd
Lucas William <lucaswillian45@gmail.com>
2019-06-27 15:41:43 -0300

    Create the Profile Measures class

---
+++ src/main/java/sizebay/catalog/client/model/UserProfileMeasures.java
@@ -0,0 +1,45 @@
+package sizebay.catalog.client.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserProfileMeasures implements Serializable {
+
+    long id;
+    long profileIdentificationId;
+    int chest;
+    int waist;
+    int hip;
+    int sleeve;
+    int length;
+    int insideLeg;
+    int neck;
+    int fist;
+    int underBust;
+
+    public static UserProfileMeasures empty() {
+
+        UserProfileMeasures measures = new UserProfileMeasures();
+
+        measures.setChest(0);
+        measures.setWaist(0);
+        measures.setHip(0);
+        measures.setSleeve(0);
+        measures.setLength(0);
+        measures.setInsideLeg(0);
+        measures.setNeck(0);
+        measures.setFist(0);
+        measures.setUnderBust(0);
+
+        return measures;
+    }
+
+}
