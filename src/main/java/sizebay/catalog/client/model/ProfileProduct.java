320159a46d644bfb656f61aed5e2f4a3c7a07ac6
Lucas <lucaswillian45@gmail.com>
2019-09-01 03:16:37 -0300

    Add brandName attribute

--- src/main/java/sizebay/catalog/client/model/ProfileProduct.java
+++ src/main/java/sizebay/catalog/client/model/ProfileProduct.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 public class ProfileProduct implements Serializable {
 	
       String brand;
+      String brandName;
       String category;
       String model;
       String gender;
