8587ea403eaed6c044163547da5aa2eb6294ec70
Lucas <lucaswillian45@gmail.com>
2019-09-01 03:17:07 -0300

    Refactor UserProfile class

--- src/main/java/sizebay/catalog/client/model/UserProfile.java
+++ src/main/java/sizebay/catalog/client/model/UserProfile.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.val;
+import sizebay.catalog.client.http.ApiException;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -26,19 +29,13 @@ public class UserProfile implements Serializable {
 
   List<Profile> profiles = new ArrayList<>();
 
-  public void insertAndUpdateProfilesList(Profile newProfile) {
-    final long profileId = newProfile.getId();
-    final boolean removeProfile = profileId != 0;
-
-    if(removeProfile) this.getProfiles().removeIf(p -> p.getId() == profileId);
-
+  public void insertProfileInProfileList(Profile newProfile) {
     newProfile.generateIdentifier();
-
     this.getProfiles().add(newProfile);
-    this.activeProfile(newProfile.getId());
+    this.activeProfileById(newProfile.getId());
   }
 
-  public void activeProfile(long profileId) {
+  public void activeProfileById(long profileId) {
     final List<Profile> profiles = this.getProfiles();
 
     profiles.stream().forEach(p -> {
@@ -51,35 +48,96 @@ public class UserProfile implements Serializable {
     });
   }
 
-  public Profile retrieveActiveProfile() {
-    final List<Profile> userProfiles = this.getProfiles();
+  public void activeProfileByGender(String gender) {
 
-    if(isNull(userProfiles)) return null;
+    if(!gender.equals("M") && !gender.equals("F") && !gender.equals("U"))
+      throw new ApiException("Unspecified genre. Enter M for men, F for women and U for unisex");
 
-    return userProfiles.stream().filter(p -> p.getIsActive() == ACTIVE).findAny().orElse(null);
+    if(!isNull(this.getProfiles())) {
+
+      val profile = this.applyGenderFilter(gender);
+
+      if(!isNull(profile)) this.activeProfileById(profile.getId());
+    }
   }
 
-  public Profile retrieveActiveProfileByGender(String gender) {
-    final List<Profile> userProfiles = this.getProfiles();
+  public Profile retrieveActiveProfile() {
+    val userProfiles = this.getProfiles();
 
     if(isNull(userProfiles)) return null;
 
-    Profile profile = retrieveActiveProfile();
-
-    if(profile.getGender().equals(gender)) {
-      return profile;
-    } else {
-      return userProfiles.stream().filter(p -> p.getGender().equals(gender))
-                                  .sorted(new SortProfileByLastActiveTime())
-                                  .collect(Collectors.toList()).get(0);
-    }
-
+    return userProfiles.stream().filter(p -> p.getIsActive() == ACTIVE).findFirst().orElse(null);
   }
 
   public Long retrieveActiveProfileId() {
     final Profile userProfile = this.retrieveActiveProfile();
 
     return isNull(userProfile) ? null : userProfile.getId();
+  }
+
+  public void activeProfileBy(String gender, Product.AgeGroupEnum age) {
+
+    if(!gender.equals("M") && !gender.equals("F") && !gender.equals("U"))
+      throw new ApiException("Unspecified genre. Enter M for men, F for women and U for unisex");
+
+    if(!isNull(this.getProfiles())) {
+
+      val profile = this.applyGenderFilter(gender, age);
+
+      if(!isNull(profile)) this.activeProfileById(profile.getId());
+    }
+  }
+
+  public Profile applyGenderFilter(String gender, Product.AgeGroupEnum age) {
+    val userProfiles = this.getProfiles();
+    val profiles = getFilteredProfiles(gender, age, userProfiles);
+    if(profiles.size() == 0)
+      return null;
+    // throw new ApiException(404, "No profiles found in this gender");
+
+    return profiles.get(0);
+  }
+
+  private List<Profile> getFilteredProfiles(String gender, Product.AgeGroupEnum age, List<Profile> userProfiles) {
+    if (gender.equals("U")) {
+      userProfiles.sort(new SortProfileByLastActiveTime());
+      return userProfiles;
+    } else {
+      return userProfiles.stream().filter(p -> p.getGender().equals(gender) && validateAgeGroup(p, age))
+          .sorted(new SortProfileByLastActiveTime())
+          .collect(Collectors.toList());
+    }
+  }
+
+  private boolean validateAgeGroup(Profile p, Product.AgeGroupEnum age) {
+
+    if (p.getAge() > 1 && p.getAge() <= 13) {
+      return Product.AgeGroupEnum.KIDS.equals(age);
+    } else if (p.getAge() <= 1) {
+      return Product.AgeGroupEnum.INFANT.equals(age);
+    } else return Product.AgeGroupEnum.ADULT.equals(age);
+  }
+
+
+  public Profile applyGenderFilter(String gender) {
+    val userProfiles = this.getProfiles();
+    val profiles = getFilteredProfiles(gender, userProfiles);
+    if(profiles.size() == 0)
+      return null;
+      // throw new ApiException(404, "No profiles found in this gender");
+
+    return profiles.get(0);
+  }
+
+  private List<Profile> getFilteredProfiles(String gender, List<Profile> userProfiles) {
+    if (gender.equals("U")) {
+      userProfiles.sort(new SortProfileByLastActiveTime());
+      return userProfiles;
+    } else {
+      return userProfiles.stream().filter(p -> p.getGender().equals(gender))
+                                                            .sorted(new SortProfileByLastActiveTime())
+                                                            .collect(Collectors.toList());
+    }
   }
 
   public static UserProfile empty(String id, String password) {
