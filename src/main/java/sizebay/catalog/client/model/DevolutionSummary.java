8e149bedc5cf185cd541e6b3a20b52738ebf0edf
Lucas <lucaswillian45@gmail.com>
2020-02-17 14:51:27 -0300

    feat: Create constructor in devolutionSummary class

--- src/main/java/sizebay/catalog/client/model/DevolutionSummary.java
+++ src/main/java/sizebay/catalog/client/model/DevolutionSummary.java
@@ -1,17 +1,19 @@
 package sizebay.catalog.client.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class DevolutionSummary {
 	private Long id;
 	private long tenantId;
 	private int insertedReturns;
 	private int invalidReturns;
 	private int totalReturns;
+
+	public DevolutionSummary(long tenantId) {
+		this.setTenantId(tenantId);
+	}
 }
