6a04e122e7faac68aca03da7dd28ae394008500b
Lucas <lucaswillian45@gmail.com>
2020-04-15 17:45:17 -0300

    feat: create StrongCategory

---
+++ src/main/java/sizebay/catalog/client/model/StrongCategory.java
@@ -0,0 +1,16 @@
+package sizebay.catalog.client.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class StrongCategory {
+
+	private Long id;
+	private String name;
+	private String type;
+	private Long typeId;
+
+}
