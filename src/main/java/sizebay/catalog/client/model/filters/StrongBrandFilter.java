24193ec05b1dc11e09a54c6e969864856f019cb6
Lucas <lucaswillian45@gmail.com>
2020-06-08 15:58:37 -0300

    feat: add @NoArgsConstructor in StrongBrandFilter class

--- src/main/java/sizebay/catalog/client/model/filters/StrongBrandFilter.java
+++ src/main/java/sizebay/catalog/client/model/filters/StrongBrandFilter.java
@@ -2,10 +2,12 @@ package sizebay.catalog.client.model.filters;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class StrongBrandFilter extends Filter {
 
