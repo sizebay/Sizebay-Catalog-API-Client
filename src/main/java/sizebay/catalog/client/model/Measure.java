cac2423201841c3587d97a979d3d629d8085bb5f
Lucas <lucaswillian45@gmail.com>
2020-06-24 09:15:56 -0300

    feat: create Measure enum

---
+++ src/main/java/sizebay/catalog/client/model/Measure.java
@@ -0,0 +1,29 @@
+package sizebay.catalog.client.model;
+
+public enum Measure {
+
+	chest,
+	waist,
+	hip,
+	sleeve,
+	biceps,
+	length,
+	insideLeg,
+	height,
+	fist,
+	neck,
+	thigh,
+	centralSeam,
+	underBust,
+	shoulderWidth,
+	insoleLength,
+	insoleWidth,
+	waistUpper,
+	waistLower,
+	ageChart,
+	weightChart,
+	equivalence,
+	headCircumference,
+	weight;
+
+}
