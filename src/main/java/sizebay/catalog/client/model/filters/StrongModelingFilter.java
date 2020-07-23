9d7227db975edb7a461b6de2a23b0365adb406b2
Lucas <lucaswillian45@gmail.com>
2020-07-23 15:05:52 -0300

    feat: create strong modeling filter

---
+++ src/main/java/sizebay/catalog/client/model/filters/StrongModelingFilter.java
@@ -0,0 +1,16 @@
+package sizebay.catalog.client.model.filters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StrongModelingFilter extends Filter {
+
+	private String page;
+
+}
