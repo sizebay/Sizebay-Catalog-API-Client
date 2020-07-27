60750cc4efb8cbf8c77ceb9a124fb8b7a47f3da2
Lucas <lucaswillian45@gmail.com>
2020-07-27 00:42:30 -0300

    feat: add more attribure in StrongModelingFilter

--- src/main/java/sizebay/catalog/client/model/filters/StrongModelingFilter.java
+++ src/main/java/sizebay/catalog/client/model/filters/StrongModelingFilter.java
@@ -12,5 +12,15 @@ import lombok.Setter;
 public class StrongModelingFilter extends Filter {
 
 	private String page;
+	private String id;
+	private String name;
+	private String ageGroup;
+	private String gender;
+	private String strongCategoryType;
+	private String strongBrandId;
+	private String strongBrandName;
+	private String modelingSizeType;
+	private String onlyOrganicModeling;
+	private String slug;
 
 }
